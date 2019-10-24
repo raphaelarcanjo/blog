bac5159541dda780327a74f5e87c4bd3fbe60006
raphaelarcanjo <raphael_arcanjo@live.com>
2019-10-24 14:22:29 -0300

    rotas finalizadas e correçao em arquivos estaticos

--- controller/router.js
+++ controller/router.js
@@ -4,20 +4,40 @@ const app = express()
 const path = require('path')
 const fs = require('fs')
 app.use(bodyParser.json())
-app.use(express.static('views/assets'))
 
-app.get('/',(req,res)=>{
-  let header = fs.readFileSync('./views/assets/header.html')
-  let page = fs.readFileSync('./views/home.html')
-  data = header + page
-  res.send(data)
-})
+let options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['css','js','png','jpg','svg','ico'],
+  index: false,
+  maxAge: '1d',
+  redirect: false
+}
+app.use(express.static('views/assets',options))
 
-app.get('/:page',(req,res)=>{
+app.get('/*',(req,res)=>{
+  let url = req.path.split('/')
   let header = fs.readFileSync('./views/assets/header.html')
-  let page = fs.readFileSync('./views/'+req.params.page+'.html')
-  data = header + page
-  res.send(data)
+  let page = ''
+
+  if(url.length > 2) {
+    let controller = require('./'+url[1])
+    let func = url[2]
+    if(url.length > 3) {
+      controller[func](url.slice(3))
+    }
+    else controller[func]()
+  }
+  else if(url[1] != '') {
+    page = fs.readFileSync('./views/'+url[1]+'.html')
+    data = header + page
+    res.send(data)
+  }
+  else{
+    page = fs.readFileSync('./views/home.html')
+    data = header + page
+    res.send(data)
+  }
 })
 
 module.exports = {app}
