4a8b43d07eef13232645ce12cdde317a58660500
raphaelarcanjo <raphael_arcanjo@live.com>
2019-10-24 01:17:25 -0300

    Iniciando com rotas

--- controller/router.js
+++ controller/router.js
@@ -2,11 +2,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const path = require('path')
+const fs = require('fs')
 app.use(bodyParser.json())
-app.use(express.static('view/assets'))
+app.use(express.static('views/assets'))
 
 app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname,'./view/home.html'))
+  let header = fs.readFileSync('./views/assets/header.html')
+  let page = fs.readFileSync('./views/home.html')
+  data = header + page
+  res.send(data)
+})
+
+app.get('/:page',(req,res)=>{
+  let header = fs.readFileSync('./views/assets/header.html')
+  let page = fs.readFileSync('./views/'+req.params.page+'.html')
+  data = header + page
+  res.send(data)
 })
 
 module.exports = {app}
