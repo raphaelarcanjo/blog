fe6975d510925fafa97c8f7748f9a2fc62d14acb
raphaelarcanjo <raphael_arcanjo@live.com>
2020-02-12 18:32:38 -0300

    Criação de sistema de cadastro e login

--- controller/router.js
+++ controller/router.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const fs = require('fs')
+const db = require('../model/db')
 
 app.use(expressLayouts)
 
@@ -17,9 +18,31 @@ app
   .get('/cadastrar', (req,res) => res.render('pages/cadastrar'))
   .get('/sobre', (req,res) => res.render('pages/sobre'))
   .get('/contato', (req,res) => res.render('pages/contato'))
+  .get('/conteudo/:user', (req,res) => res.render('pages/conteudo', {usuario: req.params.user}))
   .post('/login', (req,res) => {
-    if (req.body.login.toLowerCase() == "admin" && req.body.senha == "123456") res.send('LOGGED')
-    else res.send('NOT LOGGED')
+    db.User.findOne({login: req.body.login.toLowerCase()}, (err, usr) => {
+      if (usr != '' && req.body.senha == usr.senha) res.redirect('/conteudo/'+usr.nome+' '+usr.sobrenome)
+      else res.render('pages/home', {erro: 'Usuário ou senha incorreto!'})
+    })
+  })
+  .post('/registrar', (req, res) => {
+    if (req.body.nome == '' || req.body.sobrenome == '' || req.body.email == '' || req.body.senha == '') res.render('pages/cadastrar', {erro: 'Todos os campos devem ser preenchidos!'})
+    db.User.findOne({login: req.body.login.toLowerCase()}, (err, usr) => {
+      if (usr != '') res.render('pages/cadastrar', {erro: 'Login já cadastrado'})
+    })
+    if (req.body.senha !== req.body.confirmar_senha) res.render('pages/cadastrar', {erro: 'A confirmação da senha não confere.'})
+    else {
+      new db.User({
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        login: req.body.login.toLowerCase(),
+        email: req.body.email,
+        senha: req.body.senha
+      }).save(err => {
+        if (err) res.render('pages/cadastrar', {erro: 'Não foi possível se cadastrar. Favor contactar o administrador!'})
+        else res.render('pages/home', {mensagem: 'Você se cadastrou com sucesso! Já pode se logar e criar o Seu Blog'})
+      })
+    }
   })
 
 module.exports = {app}
--- model/db.js
+++ model/db.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 const ObjectID = require('mongodb').ObjectID
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const dbname = 'blog'
 const dburl = 'mongodb://localhost:27017/'
@@ -9,4 +10,14 @@ mongoose.connect(dburl+dbname, {useNewUrlParser:true, useUnifiedTopology:true})
 
 const database = mongoose.connection
 
-module.exports = {ObjectID,database}
+const userSchema = new Schema ({
+    nome: String,
+    sobrenome: String,
+    email: String,
+    login: String,
+    senha: String,
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {ObjectID,database, User}
